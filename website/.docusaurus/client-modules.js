a51928b51c7bbfe926c64a285c788b06e39feac3
Faycal214 <faycal213.dz@gmail.com>
2025-12-23 02:34:53 +0100

    chore: stop tracking website build artifacts

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/home/billel/Desktop/ai_projects/optiflowx/website/node_modules/infima/dist/css/default/default.css'),
-  require('/home/billel/Desktop/ai_projects/optiflowx/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/home/billel/Desktop/ai_projects/optiflowx/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/home/billel/Desktop/ai_projects/optiflowx/website/src/css/custom.css'),
-];
