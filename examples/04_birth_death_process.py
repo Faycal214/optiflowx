28d2e4519210a3c1aa4165ba4a12384a8a2d528e
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-13 05:33:26 +0100

    docs: add birth-death worked example

---
+++ examples/04_birth_death_process.py
@@ -0,0 +1,18 @@
+"""Chapter 3: birth-death process example."""
+
+from optiflowx.stochastic import BirthDeathProcess
+
+
+process = BirthDeathProcess.linear(
+    birth_rate=0.2,
+    death_rate=0.1,
+    immigration=1.0,
+    max_state=6,
+)
+
+print("generator Q:\n", process.generator_matrix())
+print("embedded jump-chain matrix:\n", process.jump_chain_matrix())
+print("Kolmogorov derivative at p0:", process.kolmogorov_derivative([1, 0, 0, 0, 0, 0, 0]))
+
+pure = BirthDeathProcess.pure_immigration(2.0)
+print("P(X_3=4) in pure immigration:", pure.pure_immigration_probability(4, 3.0, rate=2.0))
