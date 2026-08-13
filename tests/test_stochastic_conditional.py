884a336f0d1493cc729e4ec649629e27617ea4ad
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-13 03:54:35 +0100

    test: refine conditional expectation coverage

---
+++ tests/test_stochastic_conditional.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from optiflowx.stochastic import FiniteProbabilitySpace, Partition
+
+
+def make_space():
+    return FiniteProbabilitySpace([0, 1, 2, 3], [0.25, 0.25, 0.25, 0.25])
+
+
+def test_conditional_expectation_given_discrete_variable():
+    space = make_space()
+    x = space.random_variable([1.0, 3.0, 5.0, 7.0], name="X")
+    y = space.random_variable([0.0, 0.0, 1.0, 1.0], name="Y")
+
+    cond = space.conditional_expectation_given(x, y)
+    np.testing.assert_allclose(cond.array(), [2.0, 2.0, 6.0, 6.0])
+    assert cond.expectation() == 4.0
+    assert x.expectation() == 4.0
+
+
+def test_total_expectation_and_tower_property():
+    space = make_space()
+    x = space.random_variable([1.0, 2.0, 3.0, 4.0])
+    coarse = space.partition([{0, 1}, {2, 3}])
+    fine = space.partition([{0}, {1}, {2}, {3}])
+
+    cond = space.conditional_expectation(x, coarse)
+    np.testing.assert_allclose(cond.array(), [1.5, 1.5, 3.5, 3.5])
+    tower = space.tower(x, fine, coarse)
+    np.testing.assert_allclose(tower.array(), cond.array())
+    assert space.total_expectation(x, coarse) == x.expectation()
+
+
+def test_indicator_conditional_probability_and_conditional_variance():
+    space = make_space()
+    x = space.random_variable([0.0, 1.0, 2.0, 3.0])
+    partition = space.partition([{0, 1}, {2, 3}])
+    probability = space.conditional_probability({1, 3}, partition)
+    np.testing.assert_allclose(probability.array(), [0.5, 0.5, 0.5, 0.5])
+    variance = space.conditional_variance(x, partition)
+    np.testing.assert_allclose(variance.array(), [0.25, 0.25, 0.25, 0.25])
+
+
+def test_partition_generated_by_variable():
+    space = make_space()
+    y = space.random_variable([0.0, 0.0, 1.0, 1.0])
+    partition = Partition.generated_by(y)
+    assert len(partition.blocks) == 2
