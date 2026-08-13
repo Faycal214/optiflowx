3985fa87bc39b5694b42c9d51978e39b7ec5103e
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-13 05:32:49 +0100

    feat: export PDF-aligned theory helpers

--- optiflowx/stochastic/__init__.py
+++ optiflowx/stochastic/__init__.py
@@ -6,6 +6,25 @@ from .conditional import FiniteProbabilitySpace, Partition, RandomVariable
 from .markov import MarkovChain
 from .martingale import Filtration, Martingale, StoppedProcess, StoppingTime
 from .poisson import NonHomogeneousPoissonProcess, PoissonProcess
+from .theory import (
+    conditional_characterization_error,
+    conditional_expectation_given_event,
+    conditional_probability_given_event,
+    ctmc_communication_classes,
+    ctmc_mean_return_time,
+    ctmc_stationary_from_jump_chain,
+    empirical_state_frequencies,
+    first_return_probability,
+    independent_partitions,
+    independent_random_variables,
+    mean_return_time,
+    occupation_fraction,
+    occupation_time,
+    return_probability,
+    stationary_distributions,
+    stopped_martingale,
+    transform_martingale,
+)
 
 __all__ = [
     "BirthDeathProcess",
@@ -21,4 +40,21 @@ __all__ = [
     "RandomVariable",
     "StoppedProcess",
     "StoppingTime",
+    "conditional_characterization_error",
+    "conditional_expectation_given_event",
+    "conditional_probability_given_event",
+    "ctmc_communication_classes",
+    "ctmc_mean_return_time",
+    "ctmc_stationary_from_jump_chain",
+    "empirical_state_frequencies",
+    "first_return_probability",
+    "independent_partitions",
+    "independent_random_variables",
+    "mean_return_time",
+    "occupation_fraction",
+    "occupation_time",
+    "return_probability",
+    "stationary_distributions",
+    "stopped_martingale",
+    "transform_martingale",
 ]
