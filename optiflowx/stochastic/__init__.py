0e3a8d0ba859696c0a042426833a03e59933d6ba
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-13 03:48:54 +0100

    feat: expose Poisson process classes

--- optiflowx/stochastic/__init__.py
+++ optiflowx/stochastic/__init__.py
@@ -1,5 +1,6 @@
 """Stochastic-process tools for the MSPRO Processus Aléatoires curriculum."""
 
 from .markov import MarkovChain
+from .poisson import NonHomogeneousPoissonProcess, PoissonProcess
 
-__all__ = ["MarkovChain"]
+__all__ = ["MarkovChain", "PoissonProcess", "NonHomogeneousPoissonProcess"]
