f8afe263476e8747adf6e8e9160851958e8fa3d9
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-13 03:54:12 +0100

    feat: expose all stochastic course modules

--- optiflowx/stochastic/__init__.py
+++ optiflowx/stochastic/__init__.py
@@ -1,6 +1,24 @@
 """Stochastic-process tools for the MSPRO Processus Aléatoires curriculum."""
 
+from .birth_death import BirthDeathProcess
+from .cmtc import CTMCPath, ContinuousTimeMarkovChain
+from .conditional import FiniteProbabilitySpace, Partition, RandomVariable
 from .markov import MarkovChain
+from .martingale import Filtration, Martingale, StoppedProcess, StoppingTime
 from .poisson import NonHomogeneousPoissonProcess, PoissonProcess
 
-__all__ = ["MarkovChain", "PoissonProcess", "NonHomogeneousPoissonProcess"]
+__all__ = [
+    "BirthDeathProcess",
+    "CTMCPath",
+    "ContinuousTimeMarkovChain",
+    "FiniteProbabilitySpace",
+    "Filtration",
+    "MarkovChain",
+    "Martingale",
+    "NonHomogeneousPoissonProcess",
+    "Partition",
+    "PoissonProcess",
+    "RandomVariable",
+    "StoppedProcess",
+    "StoppingTime",
+]
