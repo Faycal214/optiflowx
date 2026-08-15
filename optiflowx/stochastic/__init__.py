354e96d79e60fc632ed85fda9575645d9ff74ee4
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-15 04:05:37 +0100

    refactor: remove theory helpers from public namespace

--- optiflowx/stochastic/__init__.py
+++ optiflowx/stochastic/__init__.py
@@ -1,38 +1,17 @@
-"""Public stochastic-process API for the MSPRO Processus Aléatoires curriculum.
+"""Public stochastic-process API for the MSPRO Processus Aléatoires curriculum."""
 
-Domain objects are implemented directly in their canonical modules.
-"""
-
+from .analysis import empirical_state_frequencies
 from .birth_death_process import BirthDeathProcess
 from .continuous_time_markov_chain import CTMCPath, ContinuousTimeMarkovChain
 from .filtration import Filtration
+from .markov_chain import MarkovChain
 from .martingale import Martingale
 from .partition import Partition
 from .poisson_process import NonHomogeneousPoissonProcess, PoissonProcess
 from .probability_space import FiniteProbabilitySpace
 from .random_variable import RandomVariable
-from .markov_chain import MarkovChain
 from .stopped_process import StoppedProcess
 from .stopping_time import StoppingTime
-from .theory import (
-    conditional_characterization_error,
-    conditional_expectation_given_event,
-    conditional_probability_given_event,
-    ctmc_communication_classes,
-    ctmc_mean_return_time,
-    ctmc_stationary_from_jump_chain,
-    empirical_state_frequencies,
-    first_return_probability,
-    independent_partitions,
-    independent_random_variables,
-    mean_return_time,
-    occupation_fraction,
-    occupation_time,
-    return_probability,
-    stationary_distributions,
-    stopped_martingale,
-    transform_martingale,
-)
 
 __all__ = [
     "BirthDeathProcess",
@@ -48,21 +27,5 @@ __all__ = [
     "RandomVariable",
     "StoppedProcess",
     "StoppingTime",
-    "conditional_characterization_error",
-    "conditional_expectation_given_event",
-    "conditional_probability_given_event",
-    "ctmc_communication_classes",
-    "ctmc_mean_return_time",
-    "ctmc_stationary_from_jump_chain",
     "empirical_state_frequencies",
-    "first_return_probability",
-    "independent_partitions",
-    "independent_random_variables",
-    "mean_return_time",
-    "occupation_fraction",
-    "occupation_time",
-    "return_probability",
-    "stationary_distributions",
-    "stopped_martingale",
-    "transform_martingale",
 ]
