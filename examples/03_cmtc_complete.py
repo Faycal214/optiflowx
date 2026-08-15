479fd09c3170ef5323c94f3acbef00841d35f7a8
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-15 04:07:48 +0100

    examples: use canonical CTMC path helpers

--- examples/03_cmtc_complete.py
+++ examples/03_cmtc_complete.py
@@ -3,7 +3,6 @@
 import numpy as np
 
 from optiflowx.stochastic import ContinuousTimeMarkovChain
-from optiflowx.stochastic.theory import occupation_fraction, occupation_time
 
 Q = np.array([[-2.0, 2.0], [1.0, -1.0]])
 chain = ContinuousTimeMarkovChain(Q, states=["A", "B"])
@@ -17,6 +16,9 @@ print("P(1)P(2) =\n", chain.chapman_kolmogorov(1.0, 2.0))
 print("forward derivative =\n", chain.forward_derivative(2.0))
 print("backward derivative =\n", chain.backward_derivative(2.0))
 print("stationary =", chain.stationary_distribution())
+print("stationary from jump chain =", chain.stationary_distribution_from_jump_chain())
+print("communication classes =", chain.communicating_classes())
+print("mean return time A =", chain.mean_return_time("A"))
 print("long-run cost =", chain.long_run_cost([10.0, 2.0]))
 print("holding rate A =", chain.holding_rate("A"))
 print("holding time A =", chain.holding_time("A", rng=np.random.default_rng(7)))
@@ -26,5 +28,5 @@ path = chain.simulate(10.0, initial_state="A", rng=np.random.default_rng(7))
 print("jump times =", path.times)
 print("visited states =", path.states)
 print("state at 4 =", path.state_at(4.0))
-print("occupation time A =", occupation_time(path, "A", 10.0))
-print("occupation fraction A =", occupation_fraction(path, "A", 10.0))
+print("occupation time A =", path.occupation_time("A", 10.0))
+print("occupation fraction A =", path.occupation_fraction("A", 10.0))
