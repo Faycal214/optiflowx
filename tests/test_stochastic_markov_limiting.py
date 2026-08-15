2beeb445665c2f133620ed02f9b3f488a4a4fcb7
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-15 03:28:47 +0100

    test: cover limiting distribution through transient-to-ergodic class

---
+++ tests/test_stochastic_markov_limiting.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from optiflowx.stochastic import MarkovChain
+
+
+def test_limiting_distribution_single_closed_three_state_class():
+    chain = MarkovChain(
+        [
+            [0.5, 0.5, 0.0, 0.0],
+            [0.0, 0.5, 0.5, 0.0],
+            [0.0, 0.0, 0.5, 0.5],
+            [0.0, 0.5, 0.0, 0.5],
+        ],
+        states=["T", "A", "B", "C"],
+    )
+
+    expected = np.tile([0.0, 1 / 3, 1 / 3, 1 / 3], (4, 1))
+    np.testing.assert_allclose(chain.limiting_distribution(), expected)
