5e0e168bc4499a59bd30f74e6c87197f02d9ad8b
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-15 04:09:48 +0100

    examples: finish conditional expectation theory migration

--- examples/05_conditional_expectation_complete.py
+++ examples/05_conditional_expectation_complete.py
@@ -1,11 +1,6 @@
 """Complete Chapter 4 finite conditional-expectation demonstration."""
 
 from optiflowx.stochastic import FiniteProbabilitySpace, Partition
-from optiflowx.stochastic.theory import (
-    conditional_expectation_given_event,
-    conditional_probability_given_event,
-    independent_random_variables,
-)
 
 space = FiniteProbabilitySpace([1, 2, 3, 4], [0.25] * 4)
 X = space.random_variable([1.0, 2.0, 3.0, 4.0], name="X")
@@ -24,6 +19,6 @@ print("variance =", space.variance(X))
 print("conditional variance =", space.conditional_variance(X, G).array())
 print("total variance =", space.total_variance(X, G))
 print("measurable on G =", space.check_measurable(Y, G))
-print("event probability =", conditional_probability_given_event(space, {1}, {1, 2}))
-print("event conditional expectation =", conditional_expectation_given_event(space, X, {1, 2}))
-print("X independent of Y =", independent_random_variables(space, X, Y))
+print("event probability =", space.conditional_probability_given_event({1}, {1, 2}))
+print("event conditional expectation =", space.conditional_expectation_given_event(X, {1, 2}))
+print("X independent of Y =", space.are_independent(X, Y))
