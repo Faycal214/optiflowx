15b7bb3076f9cb978c9e7eefb53dd22de8209eba
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-15 03:02:05 +0100

    refactor: enrich canonical random-variable API

--- optiflowx/stochastic/random_variable.py
+++ optiflowx/stochastic/random_variable.py
@@ -1,5 +1,19 @@
 """Public API for discrete random variables from MSPRO Chapter 4."""
 
-from .conditional import RandomVariable
+from .conditional import RandomVariable as _RandomVariable
+
+
+class RandomVariable(_RandomVariable):
+    """Finite discrete random variable defined on a finite probability space."""
+
+    @property
+    def support(self):
+        """Return the distinct values taken by the random variable."""
+        return tuple(dict.fromkeys(self.values.values()))
+
+    def expected_value(self) -> float:
+        """Alias for ``expectation()`` using a conventional statistical name."""
+        return self.expectation()
+
 
 __all__ = ["RandomVariable"]
