e299319c7efeb465a9a78898355fdd9e394ba218
faycal_afb <124316539+Faycal214@users.noreply.github.com>
2026-08-20 03:02:31 +0100

    feat Stage 9.6 public forecast API

--- stochx/timeseries/__init__.py
+++ stochx/timeseries/__init__.py
@@ -44,6 +44,7 @@ from .workfile import Workfile
 from .box_jenkins_estimation import BoxJenkinsEstimationResult, EstimatedCandidate, estimate_box_jenkins_candidates
 from .box_jenkins_validation import CandidateValidation, BoxJenkinsValidationResult, validate_box_jenkins_candidates
 from .box_jenkins_selection import BoxJenkinsSelectionResult, select_box_jenkins_model
+from .box_jenkins_forecasting import BoxJenkinsForecastResult, forecast_box_jenkins
 
 __all__ = [
     "TimeSeries", "Workfile", "Expression", "ExpressionError", "evaluate", "Equation", "EquationResult", "UnifiedResult", "ResultTable",
@@ -57,6 +58,7 @@ __all__ = [
     "EstimatedCandidate", "BoxJenkinsEstimationResult", "estimate_box_jenkins_candidates",
     "CandidateValidation", "BoxJenkinsValidationResult", "validate_box_jenkins_candidates",
     "BoxJenkinsSelectionResult", "select_box_jenkins_model",
+    "BoxJenkinsForecastResult", "forecast_box_jenkins",
     "TestResult", "durbin_watson_test", "breusch_godfrey", "breusch_godfrey_raw", "box_pierce", "ljung_box", "jarque_bera", "mean_zero_test", "normality_ks", "variance_ratio_test", "breusch_pagan", "arch_test", "residual_correlogram", "residual_diagnostics_correlogram", "residual_diagnostics", "roots_report", "redundancy_check",
     "ForecastMetrics", "metrics", "prediction_interval", "restore_differences", "naive_forecast", "drift_forecast",
     "RegressionResult", "ols", "trend_terms",
